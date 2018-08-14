7180024aed0f303fd975db26f59ba45ecc600855
MakusuMax <40814622+vloxomire@users.noreply.github.com>
2018-08-14 09:58:19 -0300

    variables
    declaro variables

--- app/src/main/java/com/beetle/dragon/oberon/OberonActivity.java
+++ app/src/main/java/com/beetle/dragon/oberon/OberonActivity.java
@@ -2,8 +2,17 @@ package com.beetle.dragon.oberon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class OberonActivity extends Activity {
+
+    private EditText et1, et2;
+    private Button button;
+    private TextView tW1;
+    private RadioButton rB1, rB2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
