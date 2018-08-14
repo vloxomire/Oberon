979c977bbb67d628d49b44c742c0c9616450c6f1
MakusuMax <40814622+vloxomire@users.noreply.github.com>
2018-08-14 09:53:56 -0300

    terminado el xml
    esta hecho el disenio inicial

---
+++ app/src/main/java/com/beetle/dragon/oberon/OberonActivity.java
@@ -0,0 +1,13 @@
+package com.beetle.dragon.oberon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OberonActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.oberon);
+    }
+}
